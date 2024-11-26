6f61eb80e6d1202b17711d12f24a7f2f6108aa32
Chris Doornink <chrisdoornink@gmail.com>
2024-11-25 17:00:21 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dice Roller",
-  description: "A simple and elegant dice roller for tabletop gaming",
+  title: "Some World",
+  description: "This is... something. Not yet, but soon, something.",
 };
 
 export default function RootLayout({
@@ -18,22 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-4QDKQY2LWD"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-4QDKQY2LWD');
-            `,
-          }}
-        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
